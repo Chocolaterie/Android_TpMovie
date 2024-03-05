11c85f0c96eeb4c1fa229d6deaa32208f72d7bae
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-03-05 15:07:08 +0100

    Correction tp 2

--- app/src/main/java/com/tp/tpmovie/MainActivity.kt
+++ app/src/main/java/com/tp/tpmovie/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.tp.tpmovie
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +14,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tp.tpmovie.ui.theme.TpMovieTheme
+import kotlin.reflect.KClass
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login);
+    }
+
+    fun openActivity(classType : KClass<*>) {
+        var intent = Intent(this, classType.java);
+        startActivity(intent);
+    }
+
+    /**
+     * Ouvrir page RegisterActivity lors d'un clique
+     */
+    fun onClickRegisterNow(view : View){
+        openActivity(RegisterActivity::class);
+    }
+
+    /**
+     * Ouvrir page RegisterActivity lors d'un clique
+     */
+    fun onClickForgetPassword(view : View){
+        openActivity(ForgettenPasswordActivity::class);
     }
 }
